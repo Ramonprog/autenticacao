f3c8e24fa3140d9d513e4b9412de6d3f0a21c32b
ramonprog <programador.ramon@gmail.com>
2022-10-28 19:16:39 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const rotas = require('./rotas')
+const PORT = 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(rotas)
+
+app.listen(PORT ,() => console.log(`Servidor rodando na porta ${PORT}`))
